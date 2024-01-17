8b0815f99edb71ccd27cb17999087cba8bdb516f
keobandolero <octav0773@gmail.com>
2024-01-17 03:34:00 -0600

    Esta es el commit final ya estan dentro todos los temas que queria y el mio propio entonces ya esta todo hasta nuevos cambios que quiera hacer

--- random_neofetch.sh
+++ random_neofetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Lista de configuraciones disponibles
-configs=("config3.conf" "config4.conf" "config6.conf" "keo.conf")
+configs=("config3.conf" "config4.conf" "ara-config.conf" "config6.conf" "keo.conf")
 
 # Seleccionar aleatoriamente una configuración
 random_config=${configs[$((RANDOM % ${#configs[@]}))]}
